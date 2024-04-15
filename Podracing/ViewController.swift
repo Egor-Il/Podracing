97882a50a0652eea06ae0bcbd6f39fcf035179b8
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2024-04-14 21:18:04 -0400

    Base for project

---
+++ Podracing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podracing
+//
+//  Created by Egor Ilchenko on 4/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
