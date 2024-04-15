ee250ce07e581642b211b100cabf7e26bc97fb69
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2024-04-14 22:40:50 -0400

    starting screen added

---
+++ Podracing/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  Podracing
+//
+//  Created by Egor Ilchenko on 4/14/24.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
--- Podracing/ViewController.swift
+++ Podracing/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var menuScreen: UIImageView!
+    @IBOutlet weak var gameNameLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var boardButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    
+    // MARK: - life cycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: - IBAction
     }
-
-
+    @IBAction func playPressed(_ sender: UIButton) {
+    }
+    @IBAction func boardPressed(_ sender: UIButton) {
+    }
+    @IBAction func settingPressed(_ sender: UIButton) {
+    }
 }
 
