6a56803fb4db8cdcc86ccb5c77137c8bd7bd1a73
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2024-04-14 23:15:29 -0400

    gaming screen added

--- Podracing/GameViewController.swift
+++ Podracing/GameViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+// MARK: - IBOutlets
+    @IBOutlet weak var trackView: UIImageView!
+    @IBOutlet weak var lefySideView: UIImageView!
+    @IBOutlet weak var rightSideView: UIImageView!
+    @IBOutlet weak var stoneOneView: UIImageView!
+    @IBOutlet weak var stoneTwoView: UIImageView!
+    @IBOutlet weak var mainPodView: UIImageView!
+    @IBOutlet weak var enemyPodView: UIImageView!
+    
+    // MARK: - life cycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
