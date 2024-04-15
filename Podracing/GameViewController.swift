3f32afad8249dcad0000cc2615a43b7db987965c
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2024-04-15 00:42:12 -0400

    movement  mechanic added

--- Podracing/GameViewController.swift
+++ Podracing/GameViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class GameViewController: UIViewController {
-// MARK: - IBOutlets
+    enum Direction{
+        case left
+        case right
+    }
+    // MARK: - IBOutlets
     @IBOutlet weak var trackView: UIImageView!
     @IBOutlet weak var lefySideView: UIImageView!
     @IBOutlet weak var rightSideView: UIImageView!
@@ -18,18 +22,28 @@ class GameViewController: UIViewController {
     @IBOutlet weak var enemyPodView: UIImageView!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
-    
     // MARK: - life cycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      // MARK: - IBAction
     }
-    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    func shipMoving(to direction:Direction) {
+        switch direction {
+        case.left:  if mainPodView.frame.origin.x > self.view.frame.origin.x - 15 {
+            mainPodView.frame.origin.x -= 15
+        }
+        case .right:
+            if mainPodView.frame.origin.x + 92 < self.view.frame.width {
+                mainPodView.frame.origin.x += 15
+            }
+        }
+    }
+    // MARK: - IBAction
     @IBAction func leftPressed(_ sender: UIButton) {
+        shipMoving(to: .left)
     }
     @IBAction func rightPressed(_ sender: UIButton) {
+        shipMoving(to: .right)
     }
-    
-
 }
---
+++ Podracing/MovingManager.swift
@@ -0,0 +1,17 @@
+//
+//  MovingManager.swift
+//  Podracing
+//
+//  Created by Egor Ilchenko on 4/14/24.
+//
+
+import Foundation
+
+final class MovingManager {
+    static let shared = MovingManager()
+    private init() {}
+    
+  
+}
+
+
