f880611c01b0ea5bf263b521381d7825bd8fba0e
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2025-04-08 18:16:40 -0400

    LeaderBoard logic done

--- Podracing/Screens/Leaderboard/LeaderboardViewController.swift
+++ Podracing/Screens/Leaderboard/LeaderboardViewController.swift
@@ -31,13 +31,13 @@ class LeaderboardViewController: UIViewController {
     private let leaderboardHeader: UIView = {
         let view = UIView()
         view.backgroundColor = .systemGray6
-       // view.alpha = 0.5
-       // view.layer.cornerRadius = 20
+        // view.alpha = 0.5
+        // view.layer.cornerRadius = 20
         return view
     }()
     var headerTextRecord: UILabel = {
         let label = UILabel()
-       // label.textAlignment = .center
+        label.textAlignment = .center
         label.font = UIFont(name: Font.fontName, size: 16)
         label.textColor = .black
         return label
@@ -51,29 +51,26 @@ class LeaderboardViewController: UIViewController {
     }()
     var headerTextDate: UILabel = {
         let label = UILabel()
-     //   label.textAlignment = .center
+        label.textAlignment = .center
         label.font = UIFont(name: Font.fontName, size: 16)
         label.textColor = .black
         return label
     }()
     private lazy var scoreTable: UITableView = {
-       let table = UITableView()
+        let table = UITableView()
         table.register(LeaderboardTableViewCell.self, forCellReuseIdentifier: LeaderboardTableViewCell.identifier)
         table.delegate = self
         table.dataSource = self
         table.backgroundColor = .clear
         table.allowsSelection = false
         table.showsVerticalScrollIndicator = false
-
-    
-       // table.separatorInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5) // - полоса разделения
+        // table.separatorInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5) // - полоса разделения
         return table
     }()
     // MARK: - Another step
     override func viewDidLoad() {
         super.viewDidLoad()
         configuratLeaderBoardUI()
-       // view.backgroundColor = .lightGray
     }
     private func configuratLeaderBoardUI() {
         view.addSubview(leaderboardImage)
@@ -81,12 +78,29 @@ class LeaderboardViewController: UIViewController {
         view.addSubview(buttonClear)
         view.addSubview(scoreTable)
         view.addSubview(leaderboardHeader)
+        
         leaderboardHeader.addSubview(headerTextRecord)
         leaderboardHeader.addSubview(headerTextName)
         leaderboardHeader.addSubview(headerTextDate)
         let  boardImage = UIImage(named: Images.leaderboard)
         leaderboardImage.image = boardImage
+        
+        setupConstraints()
         // MARK: - Property Constraints
+        
+        // MARK: - Buttons setup
+        let backfromBoardAction = UIAction { _ in
+            self.backButtonPressed()
+        }
+        let clearLeaderboardAction = UIAction { _ in
+            self.clearButtonPressed()
+        }
+        buttonBack.addAction(backfromBoardAction, for: .touchUpInside)
+        buttonClear.addAction(clearLeaderboardAction, for: .touchUpInside)
+    }
+    // MARK: - Functions
+    
+    private func setupConstraints()  {
         leaderboardImage.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
@@ -103,89 +117,54 @@ class LeaderboardViewController: UIViewController {
             make.right.equalToSuperview().inset(30)
             make.height.equalTo(50)
         }
-        headerTextRecord.snp.makeConstraints { make in
-            make.left.equalToSuperview().offset(5)
-            make.width.equalTo(80)
-            make.centerY.equalToSuperview()
-            //make.centerX.equalToSuperview()
-        }
         headerTextName.snp.makeConstraints { make in
-            make.left.equalTo(headerTextRecord.snp.right)
-            make.right.equalTo(headerTextDate.snp.left)
+            make.left.equalToSuperview().offset(5)
+            make.width.equalToSuperview().dividedBy(3)
             make.centerY.equalToSuperview()
-          //  make.centerX.equalToSuperview()
+        }
+        headerTextRecord.snp.makeConstraints { make in
+            make.left.equalTo(headerTextName.snp.right)
+            make.width.equalToSuperview().dividedBy(3)
+            make.centerY.equalToSuperview()
         }
         headerTextDate.snp.makeConstraints { make in
+            make.left.equalTo(headerTextRecord.snp.right)
             make.right.equalToSuperview()
-            make.width.equalTo(60)
             make.centerY.equalToSuperview()
-          //  make.centerX.equalToSuperview()
         }
         headerTextRecord.text = "Record"
         headerTextName.text = "Name"
         headerTextDate.text = "Date"
-        
         scoreTable.snp.makeConstraints { make in
             make.top.equalTo(leaderboardHeader.snp.bottom)
             make.left.equalToSuperview().offset(30)
             make.right.equalToSuperview().inset(30)
             make.bottom.equalToSuperview()
         }
-        // MARK: - Buttons setup
-        let backfromBoardAction = UIAction { _ in
-            self.backButtonPressed()
-        }
-        let clearLeaderboardAction = UIAction { _ in
-            self.clearButtonPressed()
-        }
-        buttonBack.addAction(backfromBoardAction, for: .touchUpInside)
-        buttonClear.addAction(clearLeaderboardAction, for: .touchUpInside)
-       // recordTime()
     }
-    // MARK: - Functions
+    
     private func backButtonPressed() {
         navigationController?.popViewController(animated: true)
     }
-    private func clearButtonPressed() {     // - надо доделать очистку таблицы. очистку ячеек
-        
-      //  print(LeaderboardData.LeaderboardDataArray)
+    private func clearButtonPressed() {
+        LeaderboardManager.shared.clearRecord()
+        scoreTable.reloadData()
     }
-    
-    func recordTime() {
-        let savedDate = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM d, yyyy, HH:mm"
-        let savedRaceDate = formatter.string(from: savedDate)
-        print(savedRaceDate)
-//       leaderboardText.text = "1 - \(savedRaceDate)"
-    }
-    
-
     // MARK: - Extension
 }
 extension LeaderboardViewController: UITableViewDelegate, UITableViewDataSource  {
     
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //    leaderboardContent.count
-      //  LeaderboardData.LeaderboardDataArray.count
         LeaderboardManager.shared.getRecord().count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: LeaderboardTableViewCell.identifier, for: indexPath) as? LeaderboardTableViewCell else {
             return UITableViewCell()
         }
-        
-        cell.fillRaceResuld()
-        
+        let entry = LeaderboardManager.shared.getRecord()[indexPath.row]
+        cell.configure(with: entry)
         cell.contentView.backgroundColor = .clear
         cell.backgroundColor = .gray
-        
-       // cell.backgroundColor = .clear // - фон ячейки
         return cell
     }
-    
-    
 }
