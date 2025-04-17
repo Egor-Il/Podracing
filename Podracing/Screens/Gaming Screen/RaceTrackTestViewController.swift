07b3cce37b29fb57d4d0d408b12cb200542a35b9
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2025-04-17 00:46:37 -0400

    New game animation

--- Podracing/Screens/Gaming Screen/RaceTrackTestViewController.swift
+++ Podracing/Screens/Gaming Screen/RaceTrackTestViewController.swift
@@ -1,71 +1,85 @@
-//
-//  RaceTrackTestViewController.swift
-//  Podracing
-//
-//  Created by Egor Ilchenko on 4/8/25.
-//
-
 import UIKit
 import SnapKit
 
 class RaceTrackTestViewController: UIViewController {
 
-    private var track: UIImageView = {
-        var trackField = UIImageView()
-        let track = UIImage(named: "v5")
-        trackField.image = track
-        return trackField
-    }()
-    private var trackTwo: UIImageView = {
-        var trackField = UIImageView()
-        let track = UIImage(named: "v5")
-        trackField.image = track
-        return trackField
-    }()
-    private var trackTree: UIImageView = {
-        var trackField = UIImageView()
-        let track = UIImage(named: "v5")
-        trackField.image = track
-        return trackField
-    }()
+    private var displayLink: CADisplayLink?
+    private var speed: CGFloat = 2.0
 
-    var trackViews: [UIView] = []
+    private let trackImages: [UIImageView] = (0..<3).map { _ in
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "trackWithCurb")
+        return imageView
+    }
+
+    private let upButton = UIButton(type: .system)
+    private let downButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        trackMovementUI()
-        trackRace()
-        //startInfiniteScroll(speed: 100)
+        setupUI()
+        startTrackAnimation()
     }
 
-    func trackMovementUI() {
-        // Устанавливаем начальные позиции для всех треков
-        [track, trackTwo, trackTree].forEach {
-            view.addSubview($0)
-            $0.snp.makeConstraints { make in
-                make.width.height.equalToSuperview()
+    private func setupUI() {
+        for track in trackImages {
+            view.addSubview(track)
+        }
+
+        view.addSubview(upButton)
+        view.addSubview(downButton)
+
+        // Расстановка кнопок
+        upButton.setTitle("UP", for: .normal)
+        upButton.backgroundColor = .systemPink
+        upButton.addTarget(self, action: #selector(increaseSpeed), for: .touchUpInside)
+
+        downButton.setTitle("Down", for: .normal)
+        downButton.backgroundColor = .systemPink
+        downButton.addTarget(self, action: #selector(decreaseSpeed), for: .touchUpInside)
+
+        upButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        downButton.snp.makeConstraints { make in
+            make.top.equalTo(upButton.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+        }
+
+        // Изначальная расстановка треков
+        for (index, track) in trackImages.enumerated() {
+            track.frame = CGRect(x: 0,
+                                 y: -view.frame.height * CGFloat(index),
+                                 width: view.frame.width,
+                                 height: view.frame.height)
+        }
+    }
+
+    private func startTrackAnimation() {
+        displayLink = CADisplayLink(target: self, selector: #selector(updateTracks))
+        displayLink?.add(to: .main, forMode: .common)
+    }
+
+    @objc private func updateTracks() {
+        for track in trackImages {
+            track.frame.origin.y += speed
+        }
+
+        for track in trackImages {
+            if track.frame.origin.y >= view.frame.height {
+                if let topY = trackImages.map({ $0.frame.origin.y }).min() {
+                    track.frame.origin.y = topY - view.frame.height
+                }
             }
         }
-
-        track.snp.makeConstraints { make in
-            make.top.equalToSuperview()  // Трек начнется с верхней части экрана
-            make.left.right.equalToSuperview()
-        }
-        trackTwo.snp.makeConstraints { make in
-            make.top.equalTo(track.snp.bottom)  // Второй трек будет под первым
-        }
-        trackTree.snp.makeConstraints { make in
-            make.top.equalTo(trackTwo.snp.bottom)  // Третий трек под вторым
-        }
-
-        trackViews = [track, trackTwo, trackTree]
     }
 
-    func trackRace() {
-        // Плавно анимируем трек вниз
-        UIView.animate(withDuration: 8, delay: 0.01, options: [.curveLinear]) {
-            // Увеличиваем y-координату, чтобы трек двигался вниз
-            self.track.frame.origin.y += self.view.frame.height
-        }
+    @objc private func increaseSpeed() {
+        speed += 1
+    }
+
+    @objc private func decreaseSpeed() {
+        speed = max(1, speed - 1)
     }
 }
